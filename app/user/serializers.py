73a1fc663d63ef1fadfc7cef7f40657a9082c5b8
shubham0420 <sharma.shubham6522@gmail.com>
2021-04-04 20:15:39 +0530

    tag list

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 from rest_framework import serializers
 
 
@@ -14,3 +14,39 @@ class UserSerializer(serializers.ModelSerializer):
         """Create a new User with Encrypted Password and return it"""
 
         return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        """Updated a user, setting the password correctly and return it"""
+
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+        return user
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    """ Serializer for User Authentication Object """
+    email = serializers.CharField()
+    password = serializers.CharField(
+        style={'input_type': 'password'}
+    )
+
+    def validate(self, attrs):
+        """Validate and authenticate the user"""
+        email = attrs.get('email')
+        password = attrs.get('password')
+
+        user = authenticate(
+            request=self.context.get('request'),
+            username=email,
+            password=password
+        )
+        if not user:
+            msg = ('Unable to authentiate with provided credentials')
+            raise serializers.ValidationError(msg, code='authencation')
+
+        attrs['user'] = user
+        return attrs
